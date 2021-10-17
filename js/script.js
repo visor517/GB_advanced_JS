033d4d4bbd4b93ec1da8fa303b232931927fbd01
visor517 <visor517@yandex.ru>
2021-10-17 15:16:36 +0300

    added basket button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,65 @@
 const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: undefined },
-    { title: undefined, price: 350 },
+    { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
 ]
+
+class BasketItem {
+    constructor({title, price}) {
+        this.title = title
+        this.price = price
+        this.quantity = 1
+    }
+    getTotalPrice() {
+        return this.price * this.quantity
+    }
+    render(index='-') { 
+        return `<tr class="goods-item">
+                    <th scope="row">${index}</th>
+                    <td>${this.title || 'нет названия'}</td>
+                    <td>${this.quantity || 0}</td>
+                    <td>${this.price || '-'} руб.</td>
+                    <td>${this.getTotalPrice() || '-'} руб.</td>
+                </tr>`
+    }
+}
+
+class Basket {
+    constructor() {
+        this.content = []
+    }
+    addItems(items) {
+        if (Array.isArray(items)) {
+            for (let item of items) {
+                this.content.push(new BasketItem(item))
+            } 
+        }
+        else this.content.push(new BasketItem(items))
+    }
+    render() {
+        let itemsList = this.content.map((item, index) => item.render(++index))
+        const tableHeader = `<tr>
+            <th scope="col">#</th>
+            <th scope="col">Название</th>
+            <th scope="col">Кол-во</th>
+            <th scope="col">Цена</th>
+            <th scope="col">Сумма</th>
+        </tr>`
+        let tableContent = itemsList.length > 0 ? itemsList.join('') : '<tr><td colspan="3">Список пуст</td></tr>'
+    
+        return tableHeader + tableContent
+    }
+}
+
+let myBasket = new Basket
+myBasket.addItems(goods)    // пока весь товар добавляем в корзину
+
+const $goodsList = document.querySelector('.goods-list')
 
 const renderGoodsItem = (index, title = 'нет названия', price = 'бесценно') => {
     return `<tr class="goods-item"><th scope="row">${index}</th><td>${title}</td><td>${price} руб.</td></tr>`
 }
-// можно сократить скобки и return, но будет длинная строка
 
 const renderGoodsList = list => {
     let goodsList = list.map((item, index) => renderGoodsItem(++index, item.title, item.price))
@@ -16,7 +67,11 @@ const renderGoodsList = list => {
 
     let tableContent = goodsList.length > 0 ? goodsList.join('') : '<tr><td colspan="3">Список пуст</td></tr>'
 
-    document.querySelector('.goods-list').innerHTML = tableHeader + tableContent
+    $goodsList.innerHTML = tableHeader + tableContent
 }
   
 renderGoodsList(goods)
+
+function renderBasket(basket) {
+    $goodsList.innerHTML = basket.render()
+}
