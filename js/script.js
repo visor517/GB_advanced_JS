134572df935a0202712bca5663c903859bd95d22
visor517 <visor517@yandex.ru>
2021-10-30 21:22:34 +0300

    hw4 done

--- js/script.js
+++ js/script.js
@@ -174,6 +174,7 @@ const $goodsList = document.querySelector('#goods-list')
 const $searchButton = document.querySelector('#search-button')
 const $searchInput = document.querySelector('#search-input')
 const $basketButton = document.querySelector('#basket-button')
+const $formButton = document.querySelector('#form-button')
 
 const list = new GoodsList()
 list.fetchGoods().then(_ => $goodsList.innerHTML = list.render())
@@ -189,4 +190,28 @@ $searchButton.addEventListener('click', e => {
 
 $basketButton.addEventListener('click', e => {
     $goodsList.innerHTML = myBasket.render()
+})
+
+$formButton.addEventListener('click', e => {
+    $formName = document.querySelector('#formName')
+    $formPhone = document.querySelector('#formPhone')
+    $formEmail = document.querySelector('#formEmail')
+    $formComment = document.querySelector('#formComment')
+
+    if (/[^a-zа-яё]/i.test($formName.value)) {
+        $formName.classList.add("border-danger")
+    }
+    else $formName.classList.remove("border-danger")
+
+    if (!/^\+7\(\d{3}\)\d{3}-\d{4}$/.test($formPhone.value)) {
+        $formPhone.classList.add("border-danger")
+    }
+    else $formPhone.classList.remove("border-danger")
+
+    if (!/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/.test($formEmail.value)) {
+        $formEmail.classList.add("border-danger")
+    }
+    else $formEmail.classList.remove("border-danger")
+
+    e.preventDefault()
 })
